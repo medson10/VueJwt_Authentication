02a652162d615895c65bcb49f7b0d2d407739f54
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-03-30 16:07:23 -0300

    Add components Login and SecretQuote

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,32 @@
 <template>
-   <nav class="navbar navbar-default">
-     <div class="container">
-       <ul class="nav navbar-nav">
-         <li><a v-link="'home'">Home</a></li>
-         <li><a v-link="'login'">Login</a></li>
-         <li><a v-link="'signup'">Sign Up</a></li>
-         <li><a v-link="'secretquote'">Secret Quote</a></li>
-         <li><a v-link="'login'">Logout</a></li>
-       </ul>
-     </div>
-   </nav>
-   <div class="container">
-     <router-view></router-view>
-   </div>
- </template>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <ul class="nav navbar-nav">
+        <li><a v-link="'home'">Home</a></li>
+        <li><a v-link="'login'" v-if="!user.authenticated">Login</a></li>
+        <li><a v-link="'signup'" v-if="!user.authenticated">Sign Up</a></li>
+        <li><a v-link="'secretquote'" v-if="user.authenticated">Secret Quote</a></li>
+        <li><a v-link="'login'" v-if="user.authenticated" @click="logout()">Logout</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import auth from '../auth'
+export default {
+  data() {
+    return {
+      user: auth.user
+    }
+  },
+  methods: {
+    logout() {
+      auth.logout()
+    }
+  }
+}
+</script>
