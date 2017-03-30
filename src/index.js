75de6923394d63eae9dbbe83251cec5b6b69e373
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-03-30 16:07:52 -0300

    Add authentication

--- src/index.js
+++ src/index.js
@@ -6,11 +6,34 @@ import Signup from './components/Signup.vue';
 import Login from './components/Login.vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import auth from './auth';
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
-export var router = new VueRouter()
+Vue.http.headers.common['Authorization'] = auth.getAuthHeader();
+
+auth.checkAuth();<template>
+   <nav class="navbar navbar-default">
+     <div class="container">
+       <ul class="nav navbar-nav">
+         <li><a v-link="'home'">Home</a></li>
+         <li><a v-link="'login'">Login</a></li>
+         <li><a v-link="'signup'">Sign Up</a></li>
+         <li><a v-link="'secretquote'">Secret Quote</a></li>
+         <li><a v-link="'login'">Logout</a></li>
+       </ul>
+     </div>
+   </nav>
+   <div class="container">
+     <router-view></router-view>
+   </div>
+ </template>
+
+
+export var router = new VueRouter();
+// Instantiate a Lock
+export var lock = new Auth0Lock(YOUR_CLIENT_ID, YOUR_CLIENT_DOMAIN);
 
 // Set up routing and match routes to components
 router.map({
